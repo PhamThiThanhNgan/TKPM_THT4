d87ddde75b002ca5eba4961106564f2323b49c2a
PhamThiThanhNgan <thanhngan1262018@gmail.com>
2022-10-01 20:52:30 +0700

    Finish Cau 9

--- src/test/java/com/example/springjpaphamthithanhngan/Repository/NhanVienRepositoryTests.java
+++ src/test/java/com/example/springjpaphamthithanhngan/Repository/NhanVienRepositoryTests.java
@@ -16,4 +16,8 @@ public class NhanVienRepositoryTests {
     void cau8(){
         System.out.println("Tổng lương phải trả cho nhân viên: " + nhanVienRepository.tinhTongLuong());
     }
+    @Test
+    void cau9(){
+        System.out.println(nhanVienRepository.getMaByLoaiMayBay());
+    }
 }
